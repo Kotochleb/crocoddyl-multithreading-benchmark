0ceec973ba521e1b76ee7dc1a680c4cea4fa4f26
Kotochleb <krzy.wojciecho@gmail.com>
2024-11-15 20:01:15 +0000

    Save data at the end of benchmark

--- experiments/pure_eigen_openmp/src/pure_eigen_openmp_pointers.cpp
+++ experiments/pure_eigen_openmp/src/pure_eigen_openmp_pointers.cpp
@@ -14,17 +14,15 @@ void benchmark_base(const unsigned int nthreads,
                     const std::string &alignment,
                     const std::size_t parallel_size,
                     const std::size_t single_core_size,
-                    const std::size_t number_of_trials) {
+                    const unsigned int number_of_trials,
+                    const std::size_t buffer_size) {
   typedef Eigen::Matrix<double, CompileMatrixSize, CompileMatrixSize>
       MatrixType;
   typedef std::shared_ptr<MatrixType> MatrixTypePtr;
-  const std::string sync_mem_str = sync_mem ? "true" : "false";
 
-  CsvStream csv(csv_filename);
-  csv << "nthreads"
-      << "sync_mem"
-      << "alignment"
-      << "time" << csv.endl;
+  // Data buffers for timings
+  std::vector<double> timings(number_of_trials);
+  std::vector<double> timing_buffer(buffer_size);
 
   // Prepare data for parallel computation
   std::vector<MatrixTypePtr> parallel_data(parallel_size);
@@ -58,64 +56,78 @@ void benchmark_base(const unsigned int nthreads,
   crocoddyl::Timer timer;
 
   // Construct problems on single core
-  for (std::size_t i = 0; i < number_of_trials; i++) {
+  for (std::size_t i = 0; i < number_of_trials; i += buffer_size) {
+    for (std::size_t j = 0; j < buffer_size; j++) {
 // Solve problem in parallel
 #pragma omp parallel for num_threads(nthreads) firstprivate(eps)
-    for (std::size_t j = 0; j < N; j++) {
-      *(parallel_data[j]) = MatrixType::Random(N, N);
-      (*(sym_tmp[j])).noalias() =
-          *(parallel_data[j]) * (*(parallel_data)[j]).transpose();
-      *(parallel_data[j]) = *(sym_tmp[j]) + eps;
-      parallel_ltt[j].compute(*(parallel_data[j]));
-    }
+      for (std::size_t k = 0; k < N; k++) {
+        *(parallel_data[k]) = MatrixType::Random(N, N);
+        (*(sym_tmp[k])).noalias() =
+            *(parallel_data[k]) * (*(parallel_data)[k]).transpose();
+        *(parallel_data[k]) = *(sym_tmp[k]) + eps;
+        parallel_ltt[k].compute(*(parallel_data[k]));
+      }
 
-    // Make the matrix symmetric positive definite
-    for (std::size_t j = 0; j < single_core_size; j++) {
-      *(single_core_data[j]) = MatrixType::Random(N, N);
-      single_core_sym_tmp.noalias() =
-          *(single_core_data[j]) * (*(single_core_data[j])).transpose();
-      *(single_core_data[j]) = single_core_sym_tmp + eps;
-    }
+      // Make the matrix symmetric positive definite
+      for (std::size_t k = 0; k < single_core_size; k++) {
+        *(single_core_data[k]) = MatrixType::Random(N, N);
+        single_core_sym_tmp.noalias() =
+            *(single_core_data[k]) * (*(single_core_data[k])).transpose();
+        *(single_core_data[k]) = single_core_sym_tmp + eps;
+      }
 
-    // Measure single core time
-    timer.reset();
-    // Enforce memory synchronization between single threaded and multithreaded
-    if (sync_mem) {
-      for (std ::size_t j = 0; j < single_core_size; j++) {
-        for (std::size_t k = 0; k < parallel_size; k++) {
-          *(single_core_data[j]) +=
-              *(parallel_data[k]) * 1.0 / static_cast<double>(parallel_size);
+      // Measure single core time
+      timer.reset();
+      // Enforce memory synchronization between single threaded and
+      // multithreaded
+      if (sync_mem) {
+        for (std ::size_t k = 0; k < single_core_size; k++) {
+          for (std::size_t l = 0; l < parallel_size; l++) {
+            *(single_core_data[k]) +=
+                *(parallel_data[l]) * 1.0 / static_cast<double>(parallel_size);
+          }
         }
       }
-    }
-    for (std ::size_t j = 0; j < single_core_size; j++) {
-      single_core_ltt[j].compute(*(single_core_data[j]));
+      for (std ::size_t k = 0; k < single_core_size; k++) {
+        single_core_ltt[k].compute(*(single_core_data[k]));
+      }
+
+      timing_buffer[j] = timer.get_us_duration();
     }
 
-    const double comp_time = timer.get_us_duration();
-    csv << nthreads << sync_mem_str << alignment << comp_time << csv.endl;
+    std::copy(timing_buffer.begin(), timing_buffer.end(), timings.begin() + i);
+  }
+
+  const std::string sync_mem_str = sync_mem ? "true" : "false";
+
+  // Save data to CSV
+  CsvStream csv(csv_filename);
+  csv << "nthreads"
+      << "sync_mem"
+      << "alignment"
+      << "time" << csv.endl;
+  for (const double time : timings) {
+    csv << nthreads << sync_mem_str << alignment << time << csv.endl;
   }
 }
 
 template <int CompileMatrixSize, int N>
-inline void benchmark_sync_mem(const unsigned int nthreads,
-                               const std::string &csv_filename,
-                               const bool sync_mem,
-                               const std::string &alignment,
-                               const std::size_t parallel_size,
-                               const std::size_t single_core_size,
-                               const std::size_t number_of_trials) {
+inline void benchmark_sync_mem(
+    const unsigned int nthreads, const std::string &csv_filename,
+    const bool sync_mem, const std::string &alignment,
+    const std::size_t parallel_size, const std::size_t single_core_size,
+    const unsigned int number_of_trials, const std::size_t buffer_size) {
   // Usage of templates ensures that the if statement will be ignored by
   // compiler making the code more efficient and preventing any chance of
   // incorrect branch predictions.
   if (sync_mem) {
     benchmark_base<CompileMatrixSize, N, true>(
         nthreads, csv_filename, alignment, parallel_size, single_core_size,
-        number_of_trials);
+        number_of_trials, buffer_size);
   } else {
     benchmark_base<CompileMatrixSize, N, false>(
         nthreads, csv_filename, alignment, parallel_size, single_core_size,
-        number_of_trials);
+        number_of_trials, buffer_size);
   }
 }
 
@@ -136,18 +148,20 @@ int main(int argc, char *argv[]) {
   // Define constant parameters
   const std::size_t parallel_size = 150;
   const std::size_t single_core_size = 30;
+  const std::size_t buffer_size = 20;
   const unsigned int number_of_trials = 1e2;
   // Size of matrices
   constexpr int K = 20;
 
   // Test dynamically allocated arrays vs statically allocated
   if (alignment == "dynamic") {
-    benchmark_sync_mem<Eigen::Dynamic, K>(nthreads, csv_filename, sync_mem,
-                                          alignment, parallel_size,
-                                          single_core_size, number_of_trials);
+    benchmark_sync_mem<Eigen::Dynamic, K>(
+        nthreads, csv_filename, sync_mem, alignment, parallel_size,
+        single_core_size, number_of_trials, buffer_size);
   } else if (alignment == "static") {
     benchmark_sync_mem<K, K>(nthreads, csv_filename, sync_mem, alignment,
-                             parallel_size, single_core_size, number_of_trials);
+                             parallel_size, single_core_size, number_of_trials,
+                             buffer_size);
   }
 
   return 0;
