6ed1b3b9ead2d037bb0e6a5e1b03d88c4576d39b
Kotochleb <krzy.wojciecho@gmail.com>
2024-11-18 17:38:38 +0000

    Fix lack of OpenMP | Move randomization | Change to high resolution clock

--- experiments/pure_eigen_openmp/src/pure_eigen_openmp.cpp
+++ experiments/pure_eigen_openmp/src/pure_eigen_openmp.cpp
@@ -1,3 +1,5 @@
+#define EIGEN_DONT_PARALLELIZE
+
 #include <omp.h>
 
 #include <Eigen/Dense>
@@ -26,28 +28,28 @@ void benchmark_base(
       parallel_size);
   std::vector<Eigen::LLT<MatrixType>,
               Eigen::aligned_allocator<Eigen::LLT<MatrixType>>>
-      parallel_ltt(parallel_size);
+      parallel_llt(parallel_size);
 
   // Prepare data for single core computation
   std::vector<MatrixType, Eigen::aligned_allocator<MatrixType>>
       single_core_data(single_core_size);
   std::vector<Eigen::LLT<MatrixType>,
               Eigen::aligned_allocator<Eigen::LLT<MatrixType>>>
-      single_core_ltt(single_core_size);
+      single_core_llt(single_core_size);
 
   // Create constants
   const MatrixType eps = 1e-6 * MatrixType::Identity(N, N);
 
   // Allocate data for benchmark
   for (std::size_t i = 0; i < parallel_size; i++) {
-    parallel_data[i] = MatrixType::Zero(N, N);
+    parallel_data[i] = MatrixType::Random(N, N);
     sym_tmp[i] = MatrixType::Zero(N, N);
-    parallel_ltt[i] = Eigen::LLT<MatrixType>(N);
+    parallel_llt[i] = Eigen::LLT<MatrixType>(N);
   }
 
   for (std::size_t i = 0; i < single_core_size; i++) {
-    single_core_data[i] = MatrixType::Zero(N, N);
-    single_core_ltt[i] = Eigen::LLT<MatrixType>(N);
+    single_core_data[i] = MatrixType::Random(N, N);
+    single_core_llt[i] = Eigen::LLT<MatrixType>(N);
   }
 
   // Prepare data for single core computation
@@ -58,26 +60,26 @@ void benchmark_base(
   for (std::size_t i = 0; i < number_of_trials; i += buffer_size) {
     for (std::size_t j = 0; j < buffer_size; j++) {
 // Solve problem in parallel
-#pragma omp parallel for if (enable_opm) num_threads(nthreads) \
+#pragma omp parallel for num_threads(nthreads) if (enable_opm) \
     shared(parallel_data) firstprivate(eps)
       for (std::size_t k = 0; k < parallel_size; k++) {
-        parallel_data[k] = MatrixType::Random(N, N);
         sym_tmp[k].noalias() = parallel_data[k] * parallel_data[k].transpose();
+        sym_tmp[k].normalize();
         parallel_data[k] = sym_tmp[k] + eps;
-        parallel_ltt[k].compute(parallel_data[k]);
+        parallel_llt[k].compute(parallel_data[k]);
       }
 
       // Make the matrix symmetric positive definite
       for (std::size_t k = 0; k < single_core_size; k++) {
-        single_core_data[k] = MatrixType::Random(N, N);
         single_core_sym_tmp.noalias() =
             single_core_data[k] * single_core_data[k].transpose();
+        single_core_sym_tmp.normalize();
         single_core_data[k] = single_core_sym_tmp + eps;
       }
 
       // Start measuring time
-      std::chrono::steady_clock::time_point begin =
-          std::chrono::steady_clock::now();
+      std::chrono::high_resolution_clock::time_point begin =
+          std::chrono::high_resolution_clock::now();
       // Enforce memory synchronization between single threaded and
       // multithreaded
       if (sync_mem) {
@@ -89,11 +91,11 @@ void benchmark_base(
         }
       }
       for (std ::size_t k = 0; k < single_core_size; k++) {
-        single_core_ltt[k].compute(single_core_data[k]);
+        single_core_llt[k].compute(single_core_data[k]);
       }
       // Stop measuring time
-      std::chrono::steady_clock::time_point end =
-          std::chrono::steady_clock::now();
+      std::chrono::high_resolution_clock::time_point end =
+          std::chrono::high_resolution_clock::now();
       timing_buffer[j] =
           std::chrono::duration_cast<std::chrono::microseconds>(end - begin)
               .count();
@@ -156,7 +158,7 @@ int main(int argc, char *argv[]) {
   const std::size_t parallel_size = 150;
   const std::size_t single_core_size = 30;
   const std::size_t buffer_size = 20;
-  const unsigned int number_of_trials = 1e2;
+  const unsigned int number_of_trials = 100;
   // Size of matrices
   constexpr int K = 20;
 
